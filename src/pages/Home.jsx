d20b3265e82d8687ef2f30e1ea7f455c6f761d07
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-10-27 20:40:37 -0500

    Add home layer for sidebar addition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import "./Home.css";
 import MainLayout from "../components/layouts/MainLayoutl";
 import OverviewHeader from "../components/molecules/Overview/OverviewHeader";
 
 export default function Home() {
 	return (
-		<MainLayout>
-			<OverviewHeader />
-		</MainLayout>
+		<div className="home">
+			<MainLayout>
+				<OverviewHeader />
+			</MainLayout>
+		</div>
 	);
 }
