0e5396b742b1d11f646d709d5b0a3097bf593b43
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-11-01 16:08:34 -0500

    Add transaction data.js file

---
+++ src/assets/data/transactionData.js
@@ -0,0 +1,16 @@
+export const transactionData = [
+	{
+		name: "Emma Richardson",
+		url: "src/assets/avatars/emma-richardson.jpg",
+		category: "General",
+		date: "19 Aug 2024",
+		amount: 75.5,
+	},
+	{
+		name: "Savory Bites Bistro",
+		url: "src/assets/avatars/savory-bites-bistro.jpg",
+		category: "Dinning Out",
+		date: "19 Aug 2024",
+		amount: 55.5,
+	},
+];
