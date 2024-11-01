ae3b9367547b75ff93c039d4a5efa5455532de6f
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-11-01 16:08:50 -0500

    Add transaction card for homepage

--- src/features/Overview/OverviewTransactions/OverviewTransactionCard.jsx
+++ src/features/Overview/OverviewTransactions/OverviewTransactionCard.jsx
@@ -1,6 +1,22 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import styles from "./OverviewTransactionCard.module.css";
 
-export default function OverviewTransactionCard() {
-	return <div>card</div>;
+export default function OverviewTransactionCard({ name, url, date, amount }) {
+	return (
+		<div className={styles.transaction}>
+			<div className={styles.name__container}>
+				<img
+					src={url}
+					alt="Avatar Image"
+					className={styles.transaction__image}
+				/>
+				<p className={styles.transaction__name}>{name}</p>
+			</div>
+			<div className={styles.info__container}>
+				<p className={styles.info__amount}>{amount}</p>
+				<p className={styles.info__date}>{date}</p>
+			</div>
+		</div>
+	);
 }
