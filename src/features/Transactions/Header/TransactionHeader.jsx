1af94eaf4f56b507ce5e91b1a485e6be4ef2597d
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-11-03 17:22:01 -0600

    Add TransactionHeader component and module

---
+++ src/features/Transactions/Header/TransactionHeader.jsx
@@ -0,0 +1,10 @@
+import "./TransactionHeader.module.css";
+import PageTitle from "../../../common/components/PageTitle/PageTitle";
+
+export default function TransactionHeader() {
+	return (
+		<div className="transaction-header">
+			<PageTitle title="Transactions" />
+		</div>
+	);
+}
