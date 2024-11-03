bddc5d815c6a47bf2a989ac6415f5bcbe3ee573b
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-11-03 17:03:44 -0600

    Add BillsData

---
+++ src/assets/data/recurringBillsData.js
@@ -0,0 +1,17 @@
+export const recurringBillsData = [
+	{
+		name: "Paid Bills",
+		amount: 190,
+		borderColor: "--green",
+	},
+	{
+		name: "Total Upcoming",
+		amount: 194.98,
+		borderColor: "--yellow",
+	},
+	{
+		name: "Due Soon",
+		amount: 59.98,
+		borderColor: "--cyan",
+	},
+];
