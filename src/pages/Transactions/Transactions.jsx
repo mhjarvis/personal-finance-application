4880268b5b6aaeab47eeeae6724a6084a7df00e2
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-11-04 09:47:45 -0600

    Add layout and styling page

--- src/pages/Transactions/Transactions.jsx
+++ src/pages/Transactions/Transactions.jsx
@@ -2,12 +2,14 @@ import styles from "./Transactions.module.css";
 import MainLayout from "../../layouts/MainLayout/MainLayoutl";
 import NavigationLayout from "../../layouts/NavigationLayout/NavigationLayout";
 import TransactionHeader from "../../features/Transactions/Header/TransactionHeader";
+import CardLayout from "../../layouts/CardLayout/CardLayout";
 
 export default function Transactions() {
 	return (
 		<div className={styles.transactions}>
 			<MainLayout>
 				<TransactionHeader />
+				<CardLayout />
 			</MainLayout>
 			<NavigationLayout />
 		</div>
