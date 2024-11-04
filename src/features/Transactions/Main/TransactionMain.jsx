340ff18770e2d59968624b64144b2aac37ead635
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-11-04 10:11:25 -0600

    update

--- src/features/Transactions/Main/TransactionMain.jsx
+++ src/features/Transactions/Main/TransactionMain.jsx
@@ -1,4 +1,7 @@
 /* eslint-disable react/prop-types */
+
+import styles from "./TransactionMain.module.css";
+
 export default function TransactionMain({ data }) {
-	return <div>{data[0].name}</div>;
+	return <div className={styles.container}>{data[0].name}</div>;
 }
