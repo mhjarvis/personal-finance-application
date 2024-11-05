99d28b352bf3445555b94de8dc81751e8a602979
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-11-04 18:02:00 -0600

    Update imports and transaction data

--- src/features/Transactions/Main/TransactionCard/TransactionCard.jsx
+++ src/features/Transactions/Main/TransactionCard/TransactionCard.jsx
@@ -1,20 +1,25 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import styles from "./TransactionCard.module.css";
 
 export default function TransactionCard({ data }) {
+	const [pageNumber, setPageNumber] = useState(0);
+
 	function updateAmount(num) {
-		return new Intl.NumberFormat("en-US", {
+		let val = new Intl.NumberFormat("en-US", {
 			minimumFractionDigits: 2,
 			maximumFractionDigits: 2,
 		}).format(num);
+		return val;
 	}
 
 	return (
 		<div className={styles.card}>
-			{data.map((item, i) => (
+			{data.slice(0, 10).map((item, i) => (
 				<div className={styles.card__container} key={i}>
 					<img
-						src={item.url}
+						src={item.avatar}
 						alt="Avatar Icon"
 						className={styles.card__image}
 					/>
@@ -23,7 +28,13 @@ export default function TransactionCard({ data }) {
 						<p className={styles.card__category}>{item.category}</p>
 					</div>
 					<div className={styles["card__stack--2"]}>
-						<p className={styles.card__amount}>${updateAmount(item.amount)}</p>
+						<p
+							className={styles.card__amount}
+							//update color based on pos/neg value
+							style={{ color: `${item.amount > 0 ? "var(--green)" : "black"}` }}
+						>
+							${updateAmount(item.amount)}
+						</p>
 						<p className={styles.card__date}>{item.date}</p>
 					</div>
 				</div>
--- src/features/Transactions/Main/TransactionMain.jsx
+++ src/features/Transactions/Main/TransactionMain.jsx
@@ -1,10 +1,8 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import TransactionSort from "./TransactionSort/TransactionSort";
 import TransactionCardHeader from "./TransactionCardHeader/TransactionCardHeader";
 import TransactionCard from "./TransactionCard/TransactionCard";
 import TransactionPages from "./TransactionPages/TransactionPages";
-
 import styles from "./TransactionMain.module.css";
 
 export default function TransactionMain({ data }) {
