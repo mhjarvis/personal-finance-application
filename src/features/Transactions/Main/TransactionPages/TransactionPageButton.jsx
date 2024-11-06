42cd3b8d50d6dfb5b4f8a59246fd00cfa471e497
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-11-06 15:16:09 -0600

    update

---
+++ src/features/Transactions/Main/TransactionPages/TransactionPageButton.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+import styles from "./TransactionPageButton.module.css";
+
+export default function TransactionPageButton({ content }) {
+	return <button className={styles.page__button}>{content}</button>;
+}
--- src/features/Transactions/Main/TransactionPages/TransactionPages.jsx
+++ src/features/Transactions/Main/TransactionPages/TransactionPages.jsx
@@ -1,3 +1,18 @@
+/* eslint-disable no-unused-vars */
+import TransactionPageButton from "./TransactionPageButton";
+import styles from "./TransactionPages.module.css";
+import leftCaret from "../assets/images/icon-caret-left.svg";
+import rightCaret from "../assets/images/icon-caret-right.svg";
+
 export default function TransactionPages() {
-	return <div>Transaction Pages</div>;
+	return (
+		<div className={styles.container}>
+			<button>{leftCaret}</button>
+			<div className={styles.pages__container}>
+				<TransactionPageButton content={leftCaret} />
+				{}
+				<TransactionPageButton content={rightCaret} />
+			</div>
+		</div>
+	);
 }
